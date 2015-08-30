f5f87695e268339feaf2c851fb5eeeea3e9e13cb
ybx <87212426@qq.com>
2015-08-30 23:25:11 +0800

    all

--- c/day01/var.c
+++ c/day01/var.c
@@ -5,17 +5,18 @@
 #endif
 
 int glob;
-
+int glob_2;//0
 int main(void)
 {
 	int var;
 	int c = 6;
-	static int d;
+	static int d;//0
 	int glob = 10;
 	
 	c = d + c;
 
-	printf("glob = %d, var = %d, c = %d, d = %d\n", glob, var, c, d);
+	//glob = 10, glob_2 = 0, var = 1684111414, c = 6, d = 0
+	printf("glob = %d, glob_2 = %d, var = %d, c = %d, d = %d\n", glob, glob_2 ,var, c, d);
 
 	return 0;
 }
