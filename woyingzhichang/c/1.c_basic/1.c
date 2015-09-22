b402de20785fda3a830d7c30684efa22edbd6991
ybx <87212426@qq.com>
2015-09-22 23:39:25 +0800

    printf

---
+++ woyingzhichang/c/1.c_basic/1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(int argc, const char *argv[])
+{
+	printf("%s\n",1);
+	return 0;
+}
--- woyingzhichang/c/1.c_basic/3.3.Segmentation.c
+++ woyingzhichang/c/1.c_basic/3.3.Segmentation.c
@@ -2,12 +2,15 @@
 #include <stdlib.h>
 #include <errno.h>
 
+//这个程序需要到linux 看效果
+//strerror缺少头文件,程序会认为strerror返回int，所以段错误
 int main(int argc, const char *argv[])
 {
 	FILE *fp;
 	fp = fopen("tmp.txt","r");//文件不存在返回空指针设置errno
 	if (NULL == fp) {
 		//strerror把errno 变成对应的错误信息
+		//不包含头文件相当于 printf("%s\n",1);
 		fprintf(stderr, "fopen: %s", strerror(errno));
 	}
 
