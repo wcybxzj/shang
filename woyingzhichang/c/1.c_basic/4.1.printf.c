40d81e6662167e41e9d54a2a008305be8ab2d5b2
ybx <87212426@qq.com>
2015-09-22 23:48:36 +0800

    all

--- woyingzhichang/c/1.c_basic/4.1.printf.c
+++ woyingzhichang/c/1.c_basic/4.1.printf.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+//only linux
+//[root@iZ25qdjus5wZ 1.c_basic]# ./a.out
+//haha
+//[root@iZ25qdjus5wZ 1.c_basic]# echo $?
+//5
 int main(int argc, const char *argv[])
 {
 	printf("haha\n");
