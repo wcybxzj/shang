8a7eb7812bbda7015d9cdfbdf19cd63c571c75f8
Your Name <you@example.com>
2016-07-14 03:30:17 +0800

    pthread attr

--- woyingzhichang/3.apue/7_8.parallel/8.thread/posix/22.create.c
+++ woyingzhichang/3.apue/7_8.parallel/8.thread/posix/22.create.c
@@ -18,23 +18,23 @@ static void* func(void *ptr)
 int main(int argc, const char *argv[])
 {
 	int i,err;
-	int size= 1024*10240;
+	int size= 1024*1024*100;
 	size_t stacksize;
 
-	//pthread_attr_t attr;
-	//pthread_attr_init(&attr);
-	//err = pthread_attr_setstacksize(&attr, size);//100m
-	//if (err) {
-	//	printf("ddddd\n");
-	//	exit(0);
-	//}
-	//err = pthread_attr_getstacksize(&attr, &stacksize);//100m
-	//printf("%d", stacksize);
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+	err = pthread_attr_setstacksize(&attr, size);//100m
+	if (err) {
+		printf("ddddd\n");
+		exit(0);
+	}
+	err = pthread_attr_getstacksize(&attr, &stacksize);//100m
+	printf("stacksize:%d", stacksize);
 
 	pthread_t tids[MAX];
 	for (i = 0; i < MAX; i++) {
-		//err = pthread_create(tids+i, &attr, func, NULL);
-		err = pthread_create(tids+i, NULL, func, NULL);
+		err = pthread_create(tids+i, &attr, func, NULL);
+		//err = pthread_create(tids+i, NULL, func, NULL);
 		if (err) {
 			fprintf(stderr, "%s\n", strerror(err));
 		}
@@ -45,6 +45,7 @@ int main(int argc, const char *argv[])
 		pthread_join(tids[i], NULL);
 	}
 
+	pthread_attr_destroy(&attr);
 	return 0;
 }
 
