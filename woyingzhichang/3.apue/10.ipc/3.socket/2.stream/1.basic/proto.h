3c1188c4dc406d2dc5675038276d19d65a4030f0
Your Name <you@example.com>
2016-09-15 06:09:48 +0800

    basic tcp

---
+++ woyingzhichang/3.apue/10.ipc/3.socket/2.stream/1.basic/proto.h
@@ -0,0 +1,8 @@
+#ifndef PROTO_H__
+#define PROTO_H__
+
+#define SERVERPORT "1998"
+
+#define FMT_STAMP "%lld"
+
+#endif
---
+++ woyingzhichang/3.apue/10.ipc/3.socket/2.stream/1.basic/server.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <string.h>
+#include <time.h>
+
+#include "proto.h"
+#define IP_SIZE 16
+
+int func1(int newsd){
+	char *str = "hello";
+	if(send(newsd, str, strlen(str), 0) < 0){
+		perror("send()");
+		exit(-3);
+	}
+}
+
+int func2(int newsd){
+	char str[IP_SIZE];
+	int len;
+	len = sprintf(str, FMT_STAMP, (long long)time(NULL));
+	printf("=====len:%d======\n", len);
+	printf(FMT_STAMP, str);
+	if(send(newsd, str, len, 0) < 0){
+		perror("send()");
+		exit(-3);
+	}
+}
+
+
+int main(){
+	int sd, newsd;
+	struct sockaddr_in laddr, raddr;
+	socklen_t rlen;
+	char ip[IP_SIZE];
+
+	sd = socket(AF_INET, SOCK_STREAM, 0/*IPPROTO_TCP*/);
+	if(sd < 0){
+		perror("socket");
+		exit(0);
+	}
+
+	int val=1;
+	if(setsockopt(sd, SOL_SOCKET, SO_REUSEADDR,\
+				&val, sizeof(val)) < 0){
+		perror("setsockopt()");
+		exit(0);
+	}
+
+	laddr.sin_family = AF_INET;
+	laddr.sin_port = htons(atoi(SERVERPORT));
+	if(inet_pton(AF_INET,"0.0.0.0",&laddr.sin_addr) != 1){
+		perror("inet_pton()");
+		exit(0);
+	}
+
+	if(bind(sd, (void *)&laddr, sizeof(laddr)) < 0){
+		perror("bind()");
+		exit(0);
+	}
+
+	listen(sd, 200);
+	while (1) {
+		newsd = accept(sd, (void *)&raddr, &rlen);
+		if(newsd<0){
+			if(errno == EAGAIN ||errno == EINTR){
+				continue;
+			}
+			perror("accept()");
+			exit(-2);
+		}
+
+		if(inet_ntop(AF_INET, (void *)&raddr.sin_addr, \
+					ip, IP_SIZE) == NULL){
+			perror("inet_ntop()");
+			exit(-2);
+		}
+
+		printf("radd:%s rport:%d\n", \
+				ip, htons(raddr.sin_port));
+		//func1(newsd);
+		func2(newsd);
+		close(newsd);//如果不close fd会泄露
+
+	}
+
+	exit(0);
+}
