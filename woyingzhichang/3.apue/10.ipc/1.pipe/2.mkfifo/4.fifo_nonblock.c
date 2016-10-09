0edf5a517a018c565dba403c92cff3e74c4f3113
Your Name <you@example.com>
2016-10-09 20:31:03 +0800

    pipe fifo

--- woyingzhichang/3.apue/10.ipc/1.pipe/2.mkfifo/4.fifo_nonblock.c
+++ woyingzhichang/3.apue/10.ipc/1.pipe/2.mkfifo/4.fifo_nonblock.c
@@ -10,13 +10,93 @@
 
 #define PATHNAME    "/tmp/myfifo3"
 
-static void sig_handler(int s);
+static void sig_handler(int s){
+	if (s==13) {
+		printf("SIGPIPE\n");
+	}
+}
 
+//child: open(O_NONBLOCK|O_RDWR) success
+void open_RDWR_nonblock()
+{
+    int fd = -1, i;
+	pid_t pid;
+	remove(PATHNAME);
+    if (mkfifo(PATHNAME, 0644) < 0) {
+        perror("mkfifo()");
+        exit(1);
+    }
+	pid = fork();
+	if (pid == 0) {
+		while (1) {
+			fd = open(PATHNAME,O_NONBLOCK|O_RDWR);
+			if (fd == -1) {
+				if (errno == ENXIO) {
+					printf("child: open(O_NONBLOCK|O_RDWR) ENXIO\n");
+					sleep(1);
+				}
+			}else{
+				printf("child: open(O_NONBLOCK|O_RDWR) success\n");
+				break;
+			}
+		}
+		exit(0);
+	}else{
+		wait(NULL);
+	}
+}
+
+
+
+//child: open(O_NONBLOCK|O_RDONLY) success
+void open_RDONLY_nonblock()
+{
+    int fd = -1, i;
+	pid_t pid;
+	remove(PATHNAME);
+    if (mkfifo(PATHNAME, 0644) < 0) {
+        perror("mkfifo()");
+        exit(1);
+    }
+	pid = fork();
+	if (pid == 0) {
+		while (1) {
+			fd = open(PATHNAME,O_NONBLOCK|O_RDONLY);
+			if (fd == -1) {
+				if (errno == ENXIO) {
+					printf("child: open(O_NONBLOCK|O_RDONLY) ENXIO\n");
+					sleep(1);
+				}
+			}else{
+				printf("child: open(O_NONBLOCK|O_RDONLY) success\n");
+				break;
+			}
+		}
+		exit(0);
+	}else{
+		wait(NULL);
+	}
+}
+
+/*
+child: open(O_NONBLOCK|O_WRONLY) ENXIO
+child: open(O_NONBLOCK|O_WRONLY) ENXIO
+parent: sleep 1
+parent: sleep 1
+child: open(O_NONBLOCK|O_WRONLY) ENXIO
+parent: sleep 1
+child: open(O_NONBLOCK|O_WRONLY) ENXIO
+parent: sleep 1
+child: open(O_NONBLOCK|O_WRONLY) ENXIO
+parent: sleep 1
+child: open(O_NONBLOCK|O_WRONLY) ENXIO
+child: open(O_NONBLOCK|O_WRONLY) success
+*/
 //子进程open以写方式打开并且非阻塞 ,open返回ENXIO
 //直到父进程open以读方式,子进程open才能成功
-void open_nonblock()
+void open_WRONLY_nonblock()
 {
-    int fd = -1;
+    int fd = -1, i;
 	pid_t pid;
 	remove(PATHNAME);
     if (mkfifo(PATHNAME, 0644) < 0) {
@@ -29,17 +109,20 @@ void open_nonblock()
 			fd = open(PATHNAME,O_NONBLOCK|O_WRONLY);
 			if (fd == -1) {
 				if (errno == ENXIO) {
-					printf("open(O_NONBLOCK|O_WRONLY) ENXIO\n");
+					printf("child: open(O_NONBLOCK|O_WRONLY) ENXIO\n");
 					sleep(1);
 				}
 			}else{
-				printf("open(O_NONBLOCK|O_WRONLY) success\n");
+				printf("child: open(O_NONBLOCK|O_WRONLY) success\n");
 				break;
 			}
 		}
 		exit(0);
 	}else{
-		sleep(2);
+		for (i = 0; i < 5; i++) {
+			sleep(1);
+			printf("parent: sleep 1\n");
+		}
 		fd = open(PATHNAME,O_RDONLY);
 		if (fd < 0) {
 			perror("open(O_RDONLY) fail");
@@ -70,15 +153,8 @@ void read_nonblock()
 		while (1) {
 			fd = open(PATHNAME, O_NONBLOCK|O_RDONLY);
 			if (fd == -1) {
-				if (errno == ENXIO) {
-					printf("ENXIO\n");
-					sleep(1);
-					continue;
-				}
-				else{
-					perror("open");
-					exit(1);
-				}
+				perror("open");
+				exit(1);
 			}else{
 				break;
 			}
@@ -105,13 +181,25 @@ void read_nonblock()
 		}
 		exit(0);
 	}else{
-		//fd = open(PATHNAME,O_WRONLY);
+		fd = open(PATHNAME,O_WRONLY);
 		wait(NULL);
 	}
 }
 
-//1->2->3->4
-//必须捕捉信号SIGPIPE
+/*
+./4.fifo_nonblock 
+ENXIO
+ENXIO
+write success len:8192
+write success len:8192
+SIGPIPE
+EPIPE
+SIGPIPE
+EPIPE
+*/
+//子进程写打开时候,FIFO还没有读打开子进程open报错ENXIO
+//父进程只读open,子进程write success
+//父进程只读close(fd),子进程write时候FIFO没有人在读write引起SIGPIPE返回EPIPE
 void write_nonblock()
 {
 	int fd = -1;
@@ -126,9 +214,9 @@ void write_nonblock()
 
 	pid = fork();
 	if (pid==0) {
-		//signal(SIGPIPE, sig_handler);
+		signal(SIGPIPE, sig_handler);
 		while (1) {
-			fd = open(PATHNAME, O_NONBLOCK|O_RDONLY);
+			fd = open(PATHNAME, O_NONBLOCK|O_WRONLY);
 			if (fd==-1) {
 				if (errno == ENXIO) {
 					printf("ENXIO\n");
@@ -139,7 +227,7 @@ void write_nonblock()
 					exit(1);
 				}
 			}
-			else{//TODO
+			else{
 				break;
 			}
 		}
@@ -157,18 +245,20 @@ void write_nonblock()
 				}
 			}else{
 				printf("write success len:%d\n", len);
+				sleep(1);
 			}
 		}
 		exit(0);
 	}else{
-		//fd = open(PATHNAME,O_RDONLY);
-		//if (fd==-1) {
-		//	perror("parent open");
-		//	exit(1);
-		//}
+		sleep(1);
+		fd = open(PATHNAME,O_RDONLY);
+		if (fd==-1) {
+			perror("parent open");
+			exit(1);
+		}
 
-		//sleep(1);
-		//close(fd);
+		sleep(1);
+		close(fd);
 		wait(NULL);
 	}
 }
@@ -176,13 +266,12 @@ void write_nonblock()
 //tlpi-44.9中 非阻塞open read write的假错的简单演示
 int main (void)
 {
-	//open_nonblock();
+	//open_RDWR_nonblock();
+	open_RDONLY_nonblock();
+	//open_WRONLY_nonblock();
 	//read_nonblock();
-	write_nonblock();
+	//write_nonblock();
 	return 0;
 }
 
-static void sig_handler(int s){
-	printf("sig!!!%d\n", s);
-}
 
