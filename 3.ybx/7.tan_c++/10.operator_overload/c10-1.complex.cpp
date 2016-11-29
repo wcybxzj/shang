056db12161601db88923ece84f2306e140cdec9d
Your Name <you@example.com>
2016-11-30 02:02:37 +0800

    on 10.5

---
+++ 3.ybx/7.tan_c++/10.operator_overload/c10-1.complex.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class Complex
+{
+	public:
+		Complex(){
+			real=0;
+			imag=0;
+		}
+		Complex(double r, double i){
+			real = r;
+			imag = i;
+		}
+		Complex complex_add(const Complex &c2);
+		void display();
+	private:
+		double real;
+		double imag;
+};
+
+Complex Complex::complex_add(const Complex &c2)
+{
+	return Complex(real+c2.real, imag+c2.imag);
+}
+
+void Complex::display()
+{
+	cout<<"("<<real<<","<<imag<<"i)"<<endl;
+}
+
+int main(int argc, const char *argv[])
+{
+	Complex c1(3, 4), c2(5, 10),c3;
+	c3 = c1.complex_add(c2);
+	c1.display();
+	c2.display();
+	c3.display();
+	return 0;
+}
