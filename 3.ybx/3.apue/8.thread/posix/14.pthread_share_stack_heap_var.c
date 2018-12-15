9bef3bdf464da9c873cf373dfc6daa151efe18a7
Your Name <you@example.com>
2018-12-15 18:21:09 +0800

    work on pthread stack

--- 3.ybx/3.apue/8.thread/posix/14.pthread_share_stack_heap_var.c
+++ 3.ybx/3.apue/8.thread/posix/14.pthread_share_stack_heap_var.c
@@ -1,34 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
 
 static void* func1(void *p){
-	printf("a:%d\n",*((int*)p));//123
+	printf("fcun1() a:%d\n",*((int*)p));//123
 	*((int*)p)=456;
+	printf("func1() a:%d\n",*((int*)p));//456
+	sleep(3);
+	printf("func1() a:%d\n",*((int*)p));//456
 }
 
 static void* func2(void *p){
-	printf("a:%d\n",*((int*)p));//456
+	sleep(1);
+	printf("func2() a:%d\n",*((int*)p));//456
 	*((int*)p)=789;
 }
 
+/*
 //测试1:测试栈变量在线程之间的共享
 //结果:
-//123
-//456
-//789
+./14.pthread_share_stack_heap_var 
+fcun1() a:123
+func1() a:456
+func2() a:456
+func1() a:789
+test_stack_var() a:789
+*/
+
 void test_stack_var()
 {
 	int a =123;
 	int i;
 	pthread_t tid;
 	pthread_create(&tid, NULL, func1, &a);
-	sleep(1);
 	pthread_create(&tid, NULL, func2, &a);
-	sleep(1);
-	printf("a:%d\n",a);//789
-	pause();
-
+	sleep(5);
+	printf("test_stack_var() a:%d\n",a);
 }
 
 static void* func3(void *p){
@@ -65,7 +73,7 @@ void test_heap_var()
 //全局变量在线程之间是共享的
 int main(int argc, const char *argv[])
 {
-	//test_stack_var();
-	test_heap_var();
+	test_stack_var();
+	//test_heap_var();
 	return 0;
 }
