3a0e5285c36243de40133d6d709d8798186df26a
Your Name <you@example.com>
2018-12-17 02:13:58 +0800

    fork memory

--- 3.ybx/3.apue/6.few/25.fork_memory.c
+++ 3.ybx/3.apue/6.few/25.fork_memory.c
@@ -5,9 +5,17 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-struct man{
+struct man {
 	char name[10];
 	int sex;
+};
+
+struct book{
+	char name[10];
+};
+
+struct book book_global = {
+	.name = "book1",
 };
 
 /*
@@ -17,7 +25,7 @@ child: name:ybx, sex:1  ----子进程继承父进程的堆空间
 child: name:MM, sex:2
 parent: name:ybx, sex:1 ----说明父子进程的堆空间是完全独立的
 */
-int main(int argc, const char *argv[])
+int heap_test()
 {
 	pid_t pid;
 	struct man *p = malloc(sizeof(struct man));
@@ -44,4 +52,41 @@ int main(int argc, const char *argv[])
 	printf("parent: name:%s, sex:%d\n",p->name, p->sex);
 
 	return 0;
+}
+
+/*
+parent: name:book1
+child: name:book1 ----子进程继承父进程的全局变量
+child: name:book2
+parent: name:book1 ----说明父子进程的全局变量是完全独立的
+*/
+int globalvar_test()
+{
+	pid_t pid;
+
+	printf("parent: name:%s\n", book_global.name);
+
+	pid = fork();
+
+	if (pid<0) {
+		printf("error\n");
+		exit(0);
+	}else if(pid==0){
+		printf("child: name:%s\n", book_global.name);
+		strcpy(book_global.name, "book2");
+		printf("child: name:%s\n", book_global.name);
+		exit(0);
+	}else{
+		wait(NULL);
+	}
+
+	printf("parent: name:%s\n", book_global.name);
+
+	return 0;
+}
+
+int main(int argc, const char *argv[])
+{
+	//heap_test();
+	globalvar_test();
 }
